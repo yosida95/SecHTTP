95d7968b92227bef5db0daa1aa7b65234651c67f
LGA128 <mikujp@gmail.com>
2013-02-24 15:54:15 +0900

    fix urls

--- proxy/urls.py
+++ proxy/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='proxy/index.html'),
         name='index'),
-    url(r'^logout/', 'proxy.views.logout'),
+    url(r'^logout/$', 'proxy.views.logout'),
     url(r'^viewer/$', 'proxy.views.viewer_home'),
     url(r'^viewer/(?P<page_id>[^/?&%#]+)', 'proxy.views.viewer', name=u'viewer'),
 )
